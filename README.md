0ca120cb07f1f0acd520e4e7e4cf9a22c4cab379
7527e <aayushsrivastava.iit72@gmail.com>
2022-06-26 21:21:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ssam9l
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ssam9l)
---
+++ index.js
@@ -0,0 +1,42 @@
+// Import stylesheets
+import './style.css';
+
+// Import ThoughtSpot SDK
+import {
+  init,
+  LiveboardEmbed,
+  Action,
+  RuntimeFilterOp,
+  EmbedEvent,
+  AuthType,
+  logout,
+  HostEvent,
+} from '@thoughtspot/visual-embed-sdk';
+
+// Initialize embed configuration
+init({
+  thoughtSpotHost:
+    /*param-start-hosturl*/ 'http://localhost:3000/' /*param-end-hosturl*/,
+  authType: AuthType.None,
+  username: 'tsadmin',
+});
+
+// Instantiate class for embedding a Liveboard
+const embed = new LiveboardEmbed('#embed', {
+  frameParams: { height: '100vh' },
+  disabledActions: [Action.ShowUnderlyingData],
+  liveboardId:
+    '33248a57-cc70-4e39-9199-fb5092283381' /*param-end-liveboardFullHeight*/ /*param-end-modifyActions*/ /*param-end-liveboardId*/ /*param-end-runtimeFilters*/,
+  /*param-start-liveboardFullHeight*/
+  /*param-start-modifyActions*/
+  /*param-start-liveboardId*/
+  // vizId: "1e99d70f-c1dc-4a52-9980-cfd4d14ba6d6",
+})
+.on(EmbedEvent.Pin, payload => {
+  console.log(payload);
+})
+.render();
+
+document.getElementById('button1').addEventListener('click', () => {
+  embed.trigger('pin', {vizId: '730496d6-6903-4601-937e-2c691821af3c'});
+});
