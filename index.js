9f97cc180ba44fa211a00211c39de2a26135bfbe
7527e <aayushsrivastava.iit72@gmail.com>
2022-07-03 13:16:47 +0530

    1 file modified

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ init({
 // Instantiate class for embedding a Liveboard
 const embed = new LiveboardEmbed('#embed', {
   frameParams: { height: '100vh' },
-  disabledActions: [Action.ShowUnderlyingData],
+  // disabledActions: [Action.ShowUnderlyingData],
   liveboardId:
     '33248a57-cc70-4e39-9199-fb5092283381' /*param-end-runtimeFilters*/,
   // vizId: "1e99d70f-c1dc-4a52-9980-cfd4d14ba6d6",
